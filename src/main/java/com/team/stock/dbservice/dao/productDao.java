6d44d87a18db866115bf9dae52fcdf6c1cd923fa
liatdavid2 <liatdavid2@gmail.com>
2019-02-15 11:20:28 +0200

    delete all

--- src/main/java/com/team/stock/dbservice/dao/productDao.java
+++
@@ -1,9 +0,0 @@
-package com.team.stock.dbservice.dao;
-
-import java.util.Collection;
-
-import com.team.stock.dbservice.classes.ProductJdbc;
-
-public interface productDao {
-public Collection<ProductJdbc>findAll();
-}
